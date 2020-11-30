b7af0c9cc464fb9598f4360d108df3e8bf65806d
Cactiw <Knyazev1514@gmail.com>
2020-11-30 16:39:35 +0300

    Added multiply models

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from string_service import trim_all
 import pandas as pd
 import numpy as np
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, mean_absolute_error
-
-
-vectorizer = CountVectorizer()
 
 
 def open_xml(path: str) -> pd.DataFrame:
@@ -47,6 +46,7 @@ train_result = train_df["evaluation"]
 test_sentences = test_df["speech"]
 test_result = test_df["evaluation"]
 
+vectorizer = CountVectorizer()
 
 train_matrix = vectorizer.fit_transform(train_sentences)
 
@@ -58,14 +58,22 @@ print(train_df)
 print(test_df)
 
 
-mnb = MultinomialNB()
+models = [MultinomialNB, SVC, LinearSVC, LogisticRegression]
 
-trained_model = mnb.fit(train_matrix, train_result)
-test_got = trained_model.predict(test_matrix)
-print(test_got)
-print(compare_results(test_got, test_result) / len(test_result) * 100)
+for model_class in models:
+    model = model_class()
+    trained_model = model.fit(train_matrix, train_result)
+    test_got = trained_model.predict(test_matrix)
+    print(test_got)
+    print(compare_results(test_got, test_result) / len(test_result) * 100)
 
 
-# print("MAE score: " + str(mean_absolute_error(test_got, test_result)) + ".")
-print("Accuracy score: " + str(accuracy_score(test_got, test_result)) + ".")
-print("F-measure: " + str(f1_score(test_got, test_result, average='macro')) + ".")
+    print("Accuracy score: " + str(accuracy_score(test_got, test_result)) + ".")
+    print("F-measure: " + str(f1_score(test_got, test_result, average='macro')) + ".")
+
+    # Можно пообщаться с моделью - раскомментировать код)
+    # sent_in = None
+    # while sent_in != "0":
+    #     sent_in = input("Введите предложение (или 0, чтобы продолжить):")
+    #     input_matrix = vectorizer_test.fit_transform([sent_in])
+    #     print(trained_model.predict(input_matrix))
